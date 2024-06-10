b65a45c1b0ff030454ee493ee430a27a63e58768
26mar1982 <26mar1982@gmail.com>
2024-06-10 12:57:14 +0700

    Create ui.ts

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'English',
+  ar: 'Arabic',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.tags': 'Categories',
+    'nav.search': 'Search',
+  },
+  ar: {
+    'nav.home': 'الرئيسية',
+    'nav.tags': 'التصنيفات',
+    'nav.search': 'بحث',
+  },
+} as const;
