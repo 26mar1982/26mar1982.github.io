a16aa343828a9ce964bb41893fc389467286afda
26mar1982 <26mar1982@gmail.com>
2024-06-11 08:27:27 +0700

    Create ui.ts

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  en: 'English',
+  ar: 'عربي',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.fatwas': 'About',
+    'nav.tags': 'Twitter',
+  },
+  ar: {
+    'nav.home': 'الرئيسية',
+    'nav.fatwas': 'الفتاوى',
+    'nav.tags': 'هاشتاج',
+  },
+} as const;
